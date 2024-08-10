e243a302af7ec8dd36552e0f850f323db4161b53
Nikandr Surkov <nikandr.dev@gmail.com>
2024-08-10 12:29:29 +0700

    release20240810-update3

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,21 @@ import type { NextRequest } from 'next/server'
 import { verifyToken } from './utils/jwtUtils'
 
 export function middleware(request: NextRequest) {
-
   if (request.nextUrl.pathname.startsWith('/protected')) {
-    const token = request.cookies.get('token')?.value
-    console.log("Token: ", token)
-    if (token) {
-      console.log("Verify token? ", verifyToken(token))
-    }
+    // Get the token cookie
+    const tokenCookie = request.cookies.get('token')
+    
+    console.log("Token cookie:", tokenCookie) // This will log the entire cookie object
+    
+    // If the cookie exists, get its value
+    const token = tokenCookie?.value
+    
+    console.log("Token value:", token)
+
+    // Log all cookies
+    const allCookies = request.cookies.getAll()
+    console.log("All cookies:", allCookies)
+
     if (!token || !verifyToken(token)) {
       return NextResponse.redirect(new URL('/', request.url))
     }
