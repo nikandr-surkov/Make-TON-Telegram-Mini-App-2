bb2a44be168d9100cad9d48d70dc5719eeb2890d
Nikandr Surkov <nikandr.dev@gmail.com>
2024-08-10 14:03:58 +0700

    Initial commit of Next.js project

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import TelegramAuth from "@/components/TelegramAuth";
+import { getSession } from "@/lib";
 
 
-export default function Home() {
+export default async function Home() {
+  const session = await getSession();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold mb-8">Telegram Auth Demo</h1>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
       <TelegramAuth />
     </main>
   )
